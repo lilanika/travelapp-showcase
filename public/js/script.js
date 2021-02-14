b42bbf53cf88e3fec1df4d04a195b5c393c0cd8a
lilanika <68299182+lilanika@users.noreply.github.com>
2021-02-14 20:26:45 +0100

    erster vorschlag

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("training JS imported successfully!");
+  },
+  false
+);
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+
+/* GET home page */
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/home", (req, res, next) => {
+  res.render("home");
+});
+
+
+module.exports = router;
